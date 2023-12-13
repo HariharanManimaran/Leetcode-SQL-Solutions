e143f29845bf3ed1fc0c6d1bff2db22450de1aac
HariharanManimaran <153490621+HariharanManimaran@users.noreply.github.com>
2023-12-14 00:14:08 +0530

    Update 175-CombineTwoTables.sql

--- 175-CombineTwoTables.sql
+++ 175-CombineTwoTables.sql
@@ -1,4 +1,8 @@
+-- LeetCode Problem Number - 175 
+-- Difficulty - Easy
+
  -- This approach involves using a LEFT JOIN operation in SQL to combine records from the 'person' and 'address' tables.
+
 select firstName , lastName , city, state
 from Person p
 Left join Address a on p.personId = a.personId;
