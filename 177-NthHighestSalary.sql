c248494bf7b34a1c78ac81bbfce4b335b570de38
HariharanManimaran <153490621+HariharanManimaran@users.noreply.github.com>
2023-12-14 00:26:29 +0530

    Create 177-NthHighestSalary.sql

---
+++ 177-NthHighestSalary.sql
@@ -0,0 +1,19 @@
+-- LeetCode Problem Number - 177
+-- Difficulty - Medium
+
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+  
+    WITH cte AS (
+    SELECT *,
+    CASE WHEN dense_rank() over(ORDER BY e.salary DESC) = N THEN e.salary
+    ELSE NULL END AS SecondHighestSalary
+    FROM Employee e
+    )
+    SELECT max(SecondHighestSalary) AS SecondHighestSalary
+    FROM cte      
+  
+  );
+END;
+$$ LANGUAGE plpgsql;
