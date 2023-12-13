6911a4b19712ecc1a107b0fabcf1f5e99a0ff07e
HariharanManimaran <153490621+HariharanManimaran@users.noreply.github.com>
2023-12-14 00:42:38 +0530

    Create 180-ConsecutiveNumbers.sql

---
+++ 180-ConsecutiveNumbers.sql
@@ -0,0 +1,13 @@
+- LeetCode Problem Number - 180
+-- Difficulty - Medium
+-- PostgreSQL
+
+select d.num as ConsecutiveNums
+from (select distinct 
+case when l.num = l2.num and l.num = l3.num 
+     then l.num
+     end as num
+from Logs l 
+left join Logs l2 on l2.id = l.id+1  
+left join logs l3 on l3.id = l.id+2) as d
+where d.num is not null;
